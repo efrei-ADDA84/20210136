25863c97f2cc8aa9eeebb19533d48133dacda865
FannyC13 <94389445+FannyC13@users.noreply.github.com>
2024-04-12 17:02:27 +0200

    Prometheus

--- Tp3.js
+++ Tp3.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const axios = require('axios');
+const prometheus = require('prom-client');
 require('dotenv').config();
 
 const app = express();
 const port = 8080;
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.OPENWEATHER_API_KEY;
+
+const counter = new prometheus.Counter({
+    name: 'weather_requests_processed_total',
+    help: 'Total number of weather requests processed',
+});
+
+prometheus.register.registerMetric(counter);
 
 app.get('/', async (req, res) => {
     try {
@@ -16,6 +24,10 @@ app.get('/', async (req, res) => {
         const weatherDescription = weatherData.weather[0].description;
         const temperature = weatherData.main.temp;
         const weatherOutput = `${cityName}: ${weatherDescription}. Temperature: ${temperature}°C.`;
+
+        
+        counter.inc();
+
         res.send(weatherOutput);
     } catch (error) {
         console.error('Error fetching weather data:', error);
@@ -23,6 +35,11 @@ app.get('/', async (req, res) => {
     }
 });
 
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', prometheus.register.contentType);
+    res.end(prometheus.register.metrics());
+});
+
 app.listen(port, () => {
-    console.log(`Server listening`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
